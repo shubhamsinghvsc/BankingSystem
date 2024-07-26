06b15765957db51d23c0f88e7b1753fee70a7560
shubhamsinghvsc <shubham.s@vsoftwareconsulting.com>
2024-07-26 18:25:50 +0530

    stored user data in json file and validated

--- BankingSystem/Customer.cs
+++ BankingSystem/Customer.cs
@@ -1,9 +1,28 @@
-﻿namespace BankingSystem
+﻿using Newtonsoft.Json;
+
+namespace BankingSystem
 {
     public class Customer
     {
+        public class Users
+        {
+            public string Name { get; set; }
+            public string Email { get; set; }
+            public string Password { get; set; }
+        }
         public static void Login()
         {
+            String email;
+            String password;
+
+            string filePath = "users.json";
+            List<Users> users = new List<Users>();
+            if (File.Exists(filePath))
+            {
+                string text = File.ReadAllText(filePath);
+                users = JsonConvert.DeserializeObject<List<Users>>(text) ?? new List<Users>();
+            }
+
             Console.Clear();
             Utils.NavBar();
 
@@ -14,6 +33,92 @@
             Utils.button(15, 67, "  Log In");
             Console.CursorVisible = true;
 
+            while (true)
+            {
+                Console.SetCursorPosition(57, 14);
+                email = Console.ReadLine();
+                email = email.ToLower();
+
+                if (email.Length == 0)
+                {
+                    Console.SetCursorPosition(57, 14);
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Email is a required field!!");
+                    Console.ResetColor();
+                    Thread.Sleep(2000);
+                    Console.SetCursorPosition(57, 14);
+                    Console.WriteLine("                           ");
+
+                }
+                else if (email.EndsWith(".com") && (email.Contains('@')) && (!email.StartsWith('@')))
+                {
+
+                    if (!IsEmailExist(users, email))
+                    {
+                        Console.SetCursorPosition(57, 14);
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("You Are Not Registered !! ");
+                        Thread.Sleep(2000);
+                        Console.ResetColor();
+                        Console.SetCursorPosition(57, 14);
+                        Console.WriteLine("                             ");
+
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                }
+
+                else
+                {
+                    Console.SetCursorPosition(57, 19);
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Please enter Valid Email !!");
+                    Thread.Sleep(2000);
+                    Console.ResetColor();
+                    Console.SetCursorPosition(57, 19);
+                    Console.WriteLine("                           ");
+                }
+            }
+
+            //Password
+            while (true)
+            {
+
+                //password
+                while (true)
+                {
+                    Console.SetCursorPosition(57, 19);
+                    password = Console.ReadLine();
+                    Console.SetCursorPosition(57, 19);
+                    Console.WriteLine(new string('*', password.Length));
+                    if (password.Length == 0 || password.Length < 8)
+                    {
+                        Console.SetCursorPosition(57, 19);
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Please Enter Valid Password !!");
+                        Console.ResetColor();
+                        Thread.Sleep(1500);
+                        Console.SetCursorPosition(57, 19);
+                        Console.WriteLine("                              ");
+                    }
+                    else
+                    {
+                        if (IsPasswordMatched(users, password))
+                        {
+                            SignUp();
+                        }
+                        break;
+                    }
+
+                }
+
+
+            }
+
+
             Console.WriteLine("New Customer ...");
             Console.Write("Press Enter to Continue");
             Console.ReadLine();
@@ -21,6 +126,13 @@
 
         public static void SignUp()
         {
+
+
+            String name;
+            String email;
+            String password;
+            String confirmPassword;
+
             Console.Clear();
             Utils.NavBar();
             Console.WriteLine("Signup".PadLeft(77, ' '));
@@ -32,10 +144,15 @@
 
             Utils.button(15, 67, "  Sign Up");
 
-            Utils.button(30, 50, "button99");
-
-
             Console.CursorVisible = true;
+
+            string filePath = "users.json";
+            List<Users> users = new List<Users>();
+            if (File.Exists(filePath))
+            {
+                string text = File.ReadAllText(filePath);
+                users = JsonConvert.DeserializeObject<List<Users>>(text) ?? new List<Users>();
+            }
 
             //Name
             while (true)
@@ -45,7 +162,7 @@
 
                 Console.SetCursorPosition(57, 14);
 
-                String name = Console.ReadLine();
+                name = Console.ReadLine();
                 if (name == "\t")
                 {
                     Console.WriteLine("Tabbbbbbbbbbbbb");
@@ -74,7 +191,7 @@
             while (true)
             {
                 Console.SetCursorPosition(57, 19);
-                String email = Console.ReadLine();
+                email = Console.ReadLine();
                 email = email.ToLower();
 
                 if (email.Length == 0)
@@ -90,10 +207,21 @@
                 }
                 else if (email.EndsWith(".com") && (email.Contains('@')) && (!email.StartsWith('@')))
                 {
-
-                    Console.WriteLine("VaLID email");
+                    //Console.WriteLine("helllllllllllllllllllllllllllllllll");
+                    if (IsEmailExist(users, email))
+                    {
+                        Console.SetCursorPosition(57, 19);
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("You Are Already Registered! ");
+                        Thread.Sleep(5000);
+                        Console.ResetColor();
+                        Console.SetCursorPosition(57, 19);
+                        Console.WriteLine("                             ");
+                        Login();
+                    }
                     break;
                 }
+
                 else
                 {
                     Console.SetCursorPosition(57, 19);
@@ -109,8 +237,7 @@
             //Password
             while (true)
             {
-                String password;
-                String confirmPassword;
+
                 //password
                 while (true)
                 {
@@ -178,7 +305,20 @@
 
             }
 
+            // storing data in json file 
 
+
+            Users user = new Users
+            {
+                Name = name,
+                Email = email,
+                Password = password,
+            };
+
+            users.Add(user);
+
+            string updatedJson = JsonConvert.SerializeObject(users, Formatting.Indented);
+            File.WriteAllText(filePath, updatedJson);
 
 
             Console.Write("\nPress Enter to Continue");
@@ -202,5 +342,33 @@
             Console.Write("Press Enter to Continue");
             Console.ReadLine();
         }
+        static bool IsEmailExist(List<Users> user, string email)
+        {
+            foreach (Users user1 in user)
+            {
+                if (user1.Email == email)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        static bool IsPasswordMatched(List<Users> user, string password)
+        {
+            foreach (Users user1 in user)
+            {
+                if (user1.Password == password)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
+
 }
+
+
+
+// use REgex
